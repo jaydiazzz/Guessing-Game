414118e02600d88f82269e66210d07cfc0408703
jaydiazzz <jaydiazzz10@gmail.com>
2018-04-20 14:36:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ function randomColor() {
 
 randomColor();
 
+setTimeout(function(){
+    document.querySelector("#popup").style.display = "block";
+},15000);
+
+document.querySelector(".close1").onclick = function() {
+  document.querySelector("#popup").style.display = "none";
+}
+
 document.querySelector(".close").onclick = function() {
   document.querySelector("#modal").classList.remove("showing");
   document.querySelectorAll(".strikes div")[0].style.display = "block";
@@ -31,12 +39,12 @@ function easy() {
     document.querySelector(`.strikes>div:nth-of-type(${i})`).style.display =
       "block";
   }
-
+  
   document.querySelector(".numbers").innerHTML = " ";
-
+  
   difficulty = 10;
   var number = Math.floor(Math.random() * difficulty) + 1;
-
+  
   console.log(number);
 
   for (let i = 1; i < difficulty + 1; i++) {
@@ -59,16 +67,16 @@ function hard() {
   count = 0;
   if (count == 0) {
     let i = 0;i < 3;i++;
-
+    
     document.querySelector(`.strikes>div:nth-of-type(${i})`).style.display =
       "block";
   }
-
+  
   document.querySelector(".numbers").innerHTML = " ";
-
+  
   difficulty = 100;
   var number = Math.floor(Math.random() * difficulty) + 1;
-
+  
   console.log(number);
 
   for (let i = 1; i < difficulty + 1; i++) {
@@ -103,17 +111,17 @@ nodeBoy.onclick = function() {
     document.querySelector("#wrong").classList.remove("showing");
     console.log(count);
   }
-
+  
   else if (document.querySelector("#modal").classList.contains("showing")) {
     count = 0;
     console.log(count);
   }
-
+  
   else if (count == 3) {
     document.querySelector("#wrong").classList.add("showing");
     count = 0;
   }
-
+  
 };
 
 function remove() {
@@ -126,12 +134,12 @@ function checkKeyPressed(e) {
   if ((document.querySelector("#modal").classList.contains("showing") ||
       document.querySelector("#wrong").classList.contains("showing")) &&
     (e.keyCode == 13 || e.keyCode == 32)) {
-
+    
     document.querySelector("#modal").classList.remove("showing");
     document.querySelector("#wrong").classList.remove("showing");
     document.querySelectorAll(".strikes div")[0].style.display = "block";
     document.querySelectorAll(".strikes div")[1].style.display = "block";
     document.querySelectorAll(".strikes div")[2].style.display = "block";
-
+  
   }
 }
